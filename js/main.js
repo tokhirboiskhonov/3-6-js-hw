cf5fe45daf9c022331673003e1363930548434b0
tokhirboiskhonov <tokhir060@gmail.com>
2022-01-06 10:56:03 +0500

    Add js

---
+++ js/main.js
@@ -0,0 +1,22 @@
+alert("Welcome to our company!");
+var userName = prompt("What's your name? :" , "Steven")
+
+var userSurname = prompt ("What's your surname? :", "Paul Jobs")
+
+var age = prompt("How old are you? ", 18)
+
+var isMaried = prompt ("Are you married? :", "Yes or No")
+
+var country = prompt ("Where are you from? :", "USA" )
+
+var education = prompt ("What degree of study? :", "Bachelor or Master degree")
+
+var experience = prompt ("Do you have experience? :", "Yes or No")
+
+var plans = prompt ("What are your plans for the future? :")
+
+var car = prompt ("Can you drive a car? :", "Yes or No")
+
+var hobby = prompt ("What are your hobbies? :")
+
+alert("Thank you for your time!");
